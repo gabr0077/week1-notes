11e9f95764ad6bf2ddb7a20fa9b960ef2cc7cc30
gabr0077 <gabr007@hotmail.com>
2022-10-31 17:48:18 +0000

    acitvaty one commrnted

--- functions.js
+++ functions.js
@@ -93,13 +93,15 @@ console.log(reverseStringDemo("hello world"));
 
               // activity 1
 
-function factorial (n){
+// function factorial (n){
 
-    if ((n === 0) || (n===1)){
-        return 1;
-    } else {
-        return(n * factorial(n-1));
+//     if ((n === 0) || (n===1)){
+//         return 1;
+//     } else {
+//         return(n * factorial(n-1));
 
-    }
-}              
-console.log(factorial(33));
+//     }
+// }              
+// console.log(factorial(33));
+
+console.log (" hello week2")
