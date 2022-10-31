1a379490e8d9f7db5a4bff2f00090d87b876d119
gabr0077 <gabr007@hotmail.com>
2022-10-31 22:25:56 +0000

    acitvaty one changed

--- dotnation.js
+++ dotnation.js
@@ -1,8 +1,19 @@
-const Foday = new Date() ;
-const Birthday = new Date ("1994, 4, 7");
-// 1000 ms is equal to 1 second. 60 seconds per minute. 60 minutes per hour. 24 hours per day.
-totalDaysAlive = Math.floor ((Today-Birthday) / 1000 / 60 / 60 / 24);
-console.log(`
-Today's Date is ${Today. toLocaleDateString()}
-I was born on ${Birthday.toLocaleDateString()}.
-I have been alive so far for ${totalDaysAlive} days.`);
+// const Foday = new Date() ;
+// const Birthday = new Date ("1994, 4, 7");
+// // 1000 ms is equal to 1 second. 60 seconds per minute. 60 minutes per hour. 24 hours per day.
+// totalDaysAlive = Math.floor ((Today-Birthday) / 1000 / 60 / 60 / 24);
+// console.log(`
+// Today's Date is ${Today. toLocaleDateString()}
+// I was born on ${Birthday.toLocaleDateString()}.
+// I have been alive so far for ${totalDaysAlive} days.`);
+
+
+// activaty 1
+
+
+for (let i = 0; i <4; i++) {
+    //print each iteration
+    console.log("|    |    |");
+    console.log("|    |    |");
+    console.log("|    |    |");
+    console.log("-------------");
