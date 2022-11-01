8dd40aa4e439d283e11b1cef48cf5168cd2f8a01
gabr0077 <gabr007@hotmail.com>
2022-11-01 00:49:51 +0000

    variables tasks

--- variables.js
+++ variables.js
@@ -16,6 +16,8 @@
 // console.log(balance)
 
 
+// activity 1:
+
 let myName = " gabr"
 let myAge = 36
 let favouriteDrink = "redbull"
@@ -30,3 +32,47 @@ favouritePet = "cat"
 
 
 console.log (` hi my name is ${myName}. my age is ${myAge}. my favouritePet is ${favouritePet} . and my favouriteDrink is ${favouriteDrink}`)
+
+
+// activity2:
+
+let breakfastmeal = "egges"
+let lunchmeal = "beans"
+let dinnermeal = "pizza"
+
+console.log(`my breakfastmeal is ${breakfastmeal}, my lunchmeal is ${lunchmeal}, and my dinnermeal is ${dinnermeal}`)
+
+let breakFastMeal = "butter and apple"
+let lunchMeal = "beans"
+let dinnerMeal = "kebabs"
+console.log(`my breakfastMeal tomorrow is ${breakFastMeal}, my lunchmeal tomorrow is ${lunchmeal}, and my dinnermeal tomorrow is ${dinnermeal}`)
+
+
+// activaty 3
+
+const start_date = new Date('1985, 11,09');
+const end_date = new Date();
+let differnt = end_date-start_date;
+
+console.log(differnt);
+
+differnt= math.floor((end_date-start_date) / 1000/ 60/ 60/ 24);
+
+console.log(`start_dateis ${start_date.toLocaleTimeString()}`);
+console.log(`i have been alive for ${differnt}days`);
+
+
+
+// activty 4
+
+space1 = "X";
+space2 = "O";
+space3 = " ";
+space4 = "X";
+space5 = "X";
+space6 = " ";
+space7 = "O";
+space8 = " ";
+space9 = " ";
+
+console.log(`   |   |   \n ${space1} | ${space2} | ${space3} \n   |   |   \n----------\n   |   |   \n ${space4} | ${space5} | ${space6}  \n   |   |   \n----------\n   |   |   \n ${space7} | ${space8} | ${space9}`);
