51c50c1736d07dabaf85894e632482930c42a98a
gabr0077 <gabr007@hotmail.com>
2022-11-06 22:49:53 +0000

    while iteration loop

--- variables.js
+++ variables.js
@@ -1,74 +1,88 @@
-// console.log (" All Around The World".charAt(1).toUpperCase());
+// // console.log (" All Around The World".charAt(1).toUpperCase());
 
-// let myName = "gabr"
+// // let myName = "gabr"
 
-// console.log(myName)
+// // console.log(myName)
 
-// myName = "gabr othman"
-// console.log(myName)
+// // myName = "gabr othman"
+// // console.log(myName)
 
-// let balance = 108
-// let withdrawalAmount = 2059
-
-
-// balance = balance-withdrawalAmount
-
-// console.log(balance)
+// // let balance = 108
+// // let withdrawalAmount = 2059
 
 
-// activity 1:
+// // balance = balance-withdrawalAmount
 
-let myName = " gabr"
-let myAge = 36
-let favouriteDrink = "redbull"
-let favouritePet = "dog"
-
-console.log (` hi my name is ${myName}. my age is ${myAge}. my favouritePet is ${favouritePet}. and my favouriteDrink is ${favouriteDrink}`)
-
- myName = " julia"
- myAge = 29
-favouriteDrink = "tea"
-favouritePet = "cat"
+// // console.log(balance)
 
 
-console.log (` hi my name is ${myName}. my age is ${myAge}. my favouritePet is ${favouritePet} . and my favouriteDrink is ${favouriteDrink}`)
+// // activity 1:
+
+// let myName = " gabr"
+// let myAge = 36
+// let favouriteDrink = "redbull"
+// let favouritePet = "dog"
+
+// console.log (` hi my name is ${myName}. my age is ${myAge}. my favouritePet is ${favouritePet}. and my favouriteDrink is ${favouriteDrink}`)
+
+//  myName = " julia"
+//  myAge = 29
+// favouriteDrink = "tea"
+// favouritePet = "cat"
 
 
-// activity2:
-
-let breakfastmeal = "egges"
-let lunchmeal = "beans"
-let dinnermeal = "pizza"
-
-console.log(`my breakfastmeal is ${breakfastmeal}, my lunchmeal is ${lunchmeal}, and my dinnermeal is ${dinnermeal}`)
-
-let breakFastMeal = "butter and apple"
-let lunchMeal = "beans"
-let dinnerMeal = "kebabs"
-console.log(`my breakfastMeal tomorrow is ${breakFastMeal}, my lunchmeal tomorrow is ${lunchmeal}, and my dinnermeal tomorrow is ${dinnermeal}`)
+// console.log (` hi my name is ${myName}. my age is ${myAge}. my favouritePet is ${favouritePet} . and my favouriteDrink is ${favouriteDrink}`)
 
 
-// activaty 3
+// // activity2:
 
-let today = new Date()
-let myBirthday = new Date ("9 november 1985")
+// let breakfastmeal = "egges"
+// let lunchmeal = "beans"
+// let dinnermeal = "pizza"
 
-let alive = Math.floor((today-myBirthday)/(24*60*60*100))
+// console.log(`my breakfastmeal is ${breakfastmeal}, my lunchmeal is ${lunchmeal}, and my dinnermeal is ${dinnermeal}`)
 
-console.log(alive)
+// let breakFastMeal = "butter and apple"
+// let lunchMeal = "beans"
+// let dinnerMeal = "kebabs"
+// console.log(`my breakfastMeal tomorrow is ${breakFastMeal}, my lunchmeal tomorrow is ${lunchmeal}, and my dinnermeal tomorrow is ${dinnermeal}`)
+
+
+// // activaty 3
+
+// let today = new Date()
+// let myBirthday = new Date ("9 november 1985")
+
+// let alive = Math.floor((today-myBirthday)/(24*60*60*100))
+
+// console.log(alive)
 
 
 
-// activty 4
+// // activty 4
 
-space1 = "X";
-space2 = "O";
-space3 = " ";
-space4 = "X";
-space5 = "X";
-space6 = " ";
-space7 = "O";
-space8 = " ";
-space9 = " ";
+// space1 = "X";
+// space2 = "O";
+// space3 = " ";
+// space4 = "X";
+// space5 = "X";
+// space6 = " ";
+// space7 = "O";
+// space8 = " ";
+// space9 = " ";
 
-console.log(`   |   |   \n ${space1} | ${space2} | ${space3} \n   |   |   \n----------\n   |   |   \n ${space4} | ${space5} | ${space6}  \n   |   |   \n----------\n   |   |   \n ${space7} | ${space8} | ${space9}`);
+// console.log(`   |   |   \n ${space1} | ${space2} | ${space3} \n   |   |   \n----------\n   |   |   \n ${space4} | ${space5} | ${space6}  \n   |   |   \n----------\n   |   |   \n ${space7} | ${space8} | ${space9}`);
+
+
+
+
+
+
+
+
+let i = 1, n = 5;
+
+while (i <= n) {
+    console.log(i,n);
+    i += 1;
+}
